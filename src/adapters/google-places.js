0e573c0a93014f1e608c87c410d6d20d946c7b59
KCraw <kristopher.craw@gmail.com>
2016-02-08 17:06:17 -0600

    Generic default precision/radius

--- src/adapters/google-places.js
+++ src/adapters/google-places.js
@@ -18,42 +18,32 @@ const _roundToDecimal = function roundToDecimal(num, dec) {
 		1: 10km cluster
 		2: 1km cluster
 		3: 100m cluster
+		4: 10m cluster
 
-		Default: 2
+		Default: 4
 
 	range - specifies range of places to retrieve (in meters)
 		1..50,000
-		Default: 2.5 * 100,000 / 10^precision 
-
-	The forumla for default range (unless overridden with a specific value) guarantees that results returned for a user
-	whose true position is on the edge of the cluster contain the results that would have been returned if the users exact
-	location had been queried at range = precision sizing. 
-
-	This will allow the API to cache all results within the cluster size, even if it returns only those results within a true
-	radius of the user to the client. 
-
+		Default: 1000
 */
 export const nearby = function nearby(lat, long, options) {
 
 	var promise = new Promise((resolve, reject) => {
 
 		let key = config.google.places.key;
-		let types = config.google.places.nearby.types.join('|');
+		let types = config.google.places.types.join('|');
 
 		// Precision clusters users 
-		let precision = (options && options.precision) || 2;
-		precision = Math.round(Math.min(Math.max(precision, 1), 3)); // whole numbers between 1 and 3
+		let precision = (options && options.precision) || 4;
 
 		lat = _roundToDecimal(lat, precision);
 		long = _roundToDecimal(long, precision);
 
 		// Radius in meters
-		let radius = (options && options.radius) || 2.5 * 100000 / Math.pow(10, precision);
-		radius = Math.round(radius); // whole numbers
+		let radius = (options && options.radius) || 1000;
 
 		let url = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=${lat},${long}&radius=${radius}&types=${types}&key=${key}`;
 
-		return console.log(url);
 		request(url, (error, response, body) => {
 
 			if (error) {
@@ -68,4 +58,5 @@ export const nearby = function nearby(lat, long, options) {
 	return promise;
 
 };
+
 
